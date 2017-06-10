83eb1205b944e51b8d8155f9ad7c07d28d994ccd
{silvio-giuliani} <silvio.giuliani@gmail.com>
2017-06-10 14:00:04 -0300

    Fixed Domain Project

--- N-Layer/api/Domain/Properties/AssemblyInfo.cs
+++ N-Layer/api/Domain/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
-[assembly: Guid("e237d65f-0187-4149-8359-45f5977d5f3e")]
+[assembly: Guid("f1d593ae-f93a-4f0a-a786-13c4704429f8")]
 
 // As informações da versão de um assembly consistem nos quatro valores a seguir:
 //
