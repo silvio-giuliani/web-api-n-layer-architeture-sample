1b3766bd0d9818bd8b4bbb4a79f4529ba99130d5
Silvio Giuliani <sgiuliani@zapcorp.com.br>
2017-06-29 19:47:51 -0300

    Altered movies controller to return async task<model>. Implemented Movies View.

--- N-Layer/Presentation/Controllers/MoviesController.cs
+++ N-Layer/Presentation/Controllers/MoviesController.cs
@@ -1,6 +1,8 @@
 ﻿using Presentation.Helpers;
 using Presentation.Models;
+using System.Collections.Generic;
 using System.Configuration;
+using System.Threading.Tasks;
 using System.Web.Mvc;
 
 namespace Presentation.Controllers
@@ -10,10 +12,9 @@ namespace Presentation.Controllers
         string api = ConfigurationManager.AppSettings["ApiUri"];
 
         // GET: Movies
-        public ActionResult Index()
+        public async Task<ActionResult> Index()
         {
-
-            var model = new ApiHelper().Get<MoviesModel>();
+            var model = await new ApiHelper().Get<List<MoviesModel>>();
 
             return View(model);
         }
